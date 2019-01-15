c4e94ec325da1461b349f5f47210b3209a00636c
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-01-14 20:26:31 -0500

    initial commit

---
+++ Assignment1/src/cs3500/hw01/publication/Webpage.java
@@ -0,0 +1,33 @@
+package cs3500.hw01.publication;
+
+public class Webpage implements Publication {
+
+  private final String title, url, retrieved;
+
+  /**
+   * Constructs a {@code Book} object.
+   *
+   * @param title     the title of the webpage
+   * @param url       url of the webpage
+   * @param retrieved date of retrieval in string format
+   */
+
+  public Webpage(String title, String url, String retrieved) {
+    this.title = title;
+    this.url = url;
+    this.retrieved = retrieved;
+  }
+
+
+
+  @Override
+  public String citeApa() {
+    return this.title + ". Retrieved " + this.retrieved + ", from " + this.url + ".";
+  }
+
+  @Override
+  public String citeMla() {
+    return "\\" + this.title + ".\\ Web. " + this.retrieved + " <" + this.url + ">.";
+  }
+}
+
