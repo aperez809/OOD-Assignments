c8b494544d66c2faae45f83315525c6d28b79254
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-01-24 01:03:57 -0500

    added ~30 tests, working on move()

--- Assignment2/src/cs3500/marblesolitaire/model/hw02/MarbleSolitaireModelImpl.java
+++ Assignment2/src/cs3500/marblesolitaire/model/hw02/MarbleSolitaireModelImpl.java
@@ -1,10 +1,13 @@
 package cs3500.marblesolitaire.model.hw02;
 
+import java.util.ArrayList;
+
 /**
  * Represents the board of a Marble Solitaire game
 */
 public class MarbleSolitaireModelImpl implements MarbleSolitaireModel {
-  private int armThickness;
+  private final int armThickness;
+  private ArrayList<ArrayList<Cell>> cells;
   private int sRow;
   private int sCol;
 
@@ -12,16 +15,23 @@ public class MarbleSolitaireModelImpl implements MarbleSolitaireModel {
     this.armThickness = 3;
     this.sRow = 3;
     this.sCol = 3;
+
+    this.cells = this.makeBoard();
+    this.assignNeighbors();
   }
 
   public MarbleSolitaireModelImpl(int sRow, int sCol) {
     this.armThickness = 3;
-    if (invalidEmptyPosition(armThickness, sRow,sCol)) {
+    if (isInvalidEmptyPosition(armThickness, sRow, sCol)) {
       throw new IllegalArgumentException(
               String.format("Invalid empty cell position (%d, %d)", sRow, sCol));
     }
     this.sRow = sRow;
     this.sCol = sCol;
+
+    this.cells = this.makeBoard();
+    this.assignNeighbors();
+
   }
 
   public MarbleSolitaireModelImpl(int armThickness) {
@@ -29,6 +39,9 @@ public class MarbleSolitaireModelImpl implements MarbleSolitaireModel {
       throw new IllegalArgumentException("armThickness must be a positive, odd number.");
     }
     this.armThickness = armThickness;
+
+    this.cells = this.makeBoard();
+    this.assignNeighbors();
   }
 
   public MarbleSolitaireModelImpl(int armThickness, int sRow, int sCol) {
@@ -37,17 +50,60 @@ public class MarbleSolitaireModelImpl implements MarbleSolitaireModel {
     }
     this.armThickness = armThickness;
 
-    if (invalidEmptyPosition(armThickness, sRow, sCol)) {
+    if (isInvalidEmptyPosition(armThickness, sRow, sCol)) {
       throw new IllegalArgumentException(
               String.format("Invalid empty cell position (%d, %d)", sRow, sCol));
     }
     this.sRow = sRow;
     this.sCol = sCol;
+
+    this.cells = this.makeBoard();
+    this.assignNeighbors();
+  }
+
+  private ArrayList<ArrayList<Cell>> makeBoard() {
+
+    ArrayList<ArrayList<Cell>> board = new ArrayList<ArrayList<Cell>>();
+
+    for (int colNum = 0; colNum < this.getBoardLength(); colNum++) {
+      ArrayList<Cell> temp = new ArrayList<>();
+
+      for (int rowNum = 0; rowNum < this.getBoardLength(); rowNum++) {
+        temp.add(createProperCell(rowNum, colNum));
+      }
+
+      board.add(temp);
+    }
+
+    return board;
+  }
+
+  private Cell createProperCell(int rowNum, int colNum) {
+
+    if (isInvalidEmptyPosition(this.armThickness, rowNum, colNum)) {
+      return new Cell(rowNum, colNum, false, false);
+    }
+    else if (rowNum == sRow && colNum == sCol) {
+      return new Cell(rowNum, colNum, false, true);
+    }
+    else {
+      return new Cell(rowNum, colNum, true, true);
+    }
   }
 
   @Override
   public void move(int fromRow, int fromCol, int toRow, int toCol) throws IllegalArgumentException {
+    if (isInvalidMoveAttempted(fromRow, fromCol, toRow, toCol)) {
+      throw new IllegalArgumentException("Illegal move attempted.");
+    }
+  }
 
+  private boolean isInvalidMoveAttempted(int fromRow, int fromCol, int toRow, int toCol) {
+    return Math.abs(fromRow - toRow) != 2
+            || Math.abs(fromCol - toCol) != 2
+            || (Math.abs(fromRow - toRow) > 0 && Math.abs(fromCol - toCol) > 0)
+            || this.cells.get(toCol).get(toRow).hasMarble
+            || isInvalidEmptyPosition(this.armThickness, toRow, toCol);
   }
 
   @Override
@@ -57,50 +113,97 @@ public class MarbleSolitaireModelImpl implements MarbleSolitaireModel {
 
   @Override
   public String getGameState() {
-    //TODO: for loop running through each row. At each point print either a space, o, or _
-    //TODO: followed by a space
-    int boardLength = this.armThickness * 2 + (this.armThickness - 2);
+
     StringBuilder boardState = new StringBuilder();
 
-    for (int i = 0; i < boardLength; i++) {
-      for (int j = 0; j < boardLength; j++) {
-        if
+    for (ArrayList<Cell> arr: this.cells) {
+      for (Cell c : arr) {
+        if (!c.isOnBoard) {
+          boardState.append(" ");
+        }
+        else if (c.hasMarble) {
+          boardState.append("O");
+        }
+        else {
+          boardState.append("_");
+        }
+        boardState.append(" ");
       }
+
+      boardState.append("\n");
     }
+    boardState.deleteCharAt(boardState.length() - 1);
+
+    return boardState.toString();
   }
 
   @Override
   public int getScore() {
-    return 0;
+    int score = 0;
+
+    for (ArrayList<Cell> arr: this.cells) {
+      for (Cell c: arr) {
+        if (c.hasMarble) {
+          score++;
+        }
+      }
+    }
+    return score;
   }
 
-  private static boolean invalidEmptyPosition(int armThickness, int sRow, int sCol) {
-
-    //TODO: First 4 lines are correct and work
-    //TODO: Need to have logic for handling when the sRow value makes certain values of sCol illegal
-    //TODO: And vice versa
-    return sRow >= 0
-            && sCol >= 0
-            && sRow <= armThickness * 2 + (armThickness - 2)
-            && sCol <= armThickness * 2 + (armThickness - 2)
-            && (insideBoardPossibilities(armThickness, sRow, sCol)
-            || insideBoardPossibilities(armThickness, sCol, sRow));
-
-    //TODO: Middle piece == Math.floor(armThickness * 2 + (armThickness - 2) / 2)
-    //TODO: so if either sRow or sCol is outside the bound of middle
-    //TODO: piece +/- Math.floor(armThickness / 2
+  private static boolean isInvalidEmptyPosition(int armThickness, int sRow, int sCol) {
+    return sRow < 0
+            || sCol < 0
+            || sRow > armThickness * 2 + (armThickness - 3)
+            || sCol > armThickness * 2 + (armThickness - 3)
+            || !isInsideBoardPossibilities(armThickness, sRow, sCol);
   }
 
-  private static boolean insideBoardPossibilities(int armThickness, int a, int b) {
-    int middlePiece = (int) Math.floor(armThickness * 2 + (armThickness - 2) / 2);
-    if (a > middlePiece + (int) Math.floor(armThickness / 2)
-            || a < middlePiece - (int) Math.floor(armThickness / 2)) {
-      return b > middlePiece + (int) Math.floor(armThickness / 2)
-              || b < middlePiece - (int) Math.floor(armThickness / 2);
+  private static boolean isInsideBoardPossibilities(int armThickness,
+                                                    int freeVal,
+                                                    int constrainedVal) {
+    int middlePiece = (armThickness * 2 + (armThickness - 3)) / 2;
+    int validMargin = (armThickness - 1) / 2;
+
+    if (freeVal <= middlePiece + validMargin
+            && freeVal >= middlePiece - validMargin) {
+      return true;
     }
 
     else {
-      return true;
+      return constrainedVal <= middlePiece + validMargin
+              && constrainedVal >= middlePiece - validMargin;
     }
+  }
+
+  //uses nested loops to access elements of list of lists. Depending on which neighbor is being
+  //assigned, sets neighbor to either next or previous indexed element in same list or
+  //same indexed element in next or previous list
+  private void assignNeighbors() {
+    for (int i = 0; i < this.cells.size(); i++) {
+      for (int j = 0; j < this.cells.size(); j++) {
+        this.cells.get(i).get(j).up = this.findNeighbor(i - 1, j);
+        this.cells.get(i).get(j).down = this.findNeighbor(i + 1, j);
+        this.cells.get(i).get(j).left = this.findNeighbor(i, j - 1);
+        this.cells.get(i).get(j).right = this.findNeighbor(i, j + 1);
+      }
+    }
+  }
+
+  //confirms given i and j indexes are not outside bounds of board. If so, set neighbor to null as,
+  //as that would indicate an edge cell.
+  //Otherwise, sets neighbor to element at given index of board
+  private Cell findNeighbor(int i, int j) {
+    if (i < 0 || i >= this.cells.size()
+            || j < 0 || j >= this.cells.size()) {
+      return null;
+    }
+    else {
+      return this.cells.get(i).get(j);
+    }
+  }
+
+  private int getBoardLength() {
+    return this.armThickness * 2 + (this.armThickness - 2);
   }
 }
--- Assignment2/test/MarbleSolitaireModelImplTest.java
+++ Assignment2/test/MarbleSolitaireModelImplTest.java
@@ -1,17 +1,184 @@
+import cs3500.marblesolitaire.model.hw02.MarbleSolitaireModelImpl;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
 public class MarbleSolitaireModelImplTest {
+
   @Test
   public void testConstructorNoParam() {
-
+    new MarbleSolitaireModelImpl();
   }
 
-  @Test(expected = IllegalArgumentException)
-  public void testConstructorRowAndColParam() {
-
-
+  @Test
+  public void testConstructorRowAndColParamCorrect() {
+    new MarbleSolitaireModelImpl(2,1);
+    new MarbleSolitaireModelImpl(4,4);
   }
 
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorRowAndColParamInvalidOOB() {
+    new MarbleSolitaireModelImpl(6,6);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorRowAndColParamInvalidRowNeg() {
+    new MarbleSolitaireModelImpl(-1,6);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorRowAndColParamInvalidColNeg() {
+    new MarbleSolitaireModelImpl(6,-1);
+  }
+
+  @Test
+  public void testConstructorArmThicknessCorrect() {
+    new MarbleSolitaireModelImpl(9);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorArmThicknessInvalidEven() {
+    new MarbleSolitaireModelImpl(8);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorArmThicknessInvalidZero() {
+    new MarbleSolitaireModelImpl(0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorArmThicknessInvalidNeg() {
+    new MarbleSolitaireModelImpl(-5);
+  }
+
+  @Test
+  public void testConstructorAllPartsCorrect() {
+    new MarbleSolitaireModelImpl(7, 5, 6);
+    new MarbleSolitaireModelImpl(5, 8, 4);
+    new MarbleSolitaireModelImpl(3, 2, 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorAllPartsInvalidATEven() {
+    new MarbleSolitaireModelImpl(8, 2, 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorAllPartsInvalidATZero() {
+    new MarbleSolitaireModelImpl(0, 2, 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorAllPartsInvalidATNeg() {
+    new MarbleSolitaireModelImpl(-7, 2, 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorAllPartsInvalidRowOOB() {
+    new MarbleSolitaireModelImpl(5, 12, 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorAllPartsInvalidColOOB() {
+    new MarbleSolitaireModelImpl(5, 0, 2);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorAllPartsInvalidRowNeg() {
+    new MarbleSolitaireModelImpl(5, -1, 2);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorAllPartsInvalidColNeg() {
+    new MarbleSolitaireModelImpl(5, 0, -1);
+  }
+
+  @Test
+  public void testGetGameState() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    assertEquals("    O O O     \n" +
+                    "    O O O     \n" +
+                    "O O O O O O O \n" +
+                    "O O O _ O O O \n" +
+                    "O O O O O O O \n" +
+                    "    O O O     \n" +
+                    "    O O O     ",
+            initData.getGameState());
+  }
+
+  @Test
+  public void testGetScore() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    assertEquals(32, initData.getScore());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testValidMoveLeft() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(3, 5, 3,3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testValidMoveRight() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(3, 1, 3,3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testValidMoveUp() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(5, 3, 3,3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testValidMoveDown() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(1, 3, 3,3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidMoveIllegalStart() {
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidMoveToOccupiedSpace() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(3,2,1,2);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidMoveOffBoard() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(3,1,5,1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidMoveOverEmptySpace() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(2,3,4,3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidMoveTooManySpacesRow() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(3, 2, 6,2);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidMoveTooManySpacesCol() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(2, 3, 2,6);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidMoveTooManySpacesRowAndCol() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl();
+    initData.move(2, 3, 3,6);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidMoveDiagonalJump() {
+    MarbleSolitaireModelImpl initData = new MarbleSolitaireModelImpl(7);
+    initData.move(11, 7, 9,9);
+  }
 }
