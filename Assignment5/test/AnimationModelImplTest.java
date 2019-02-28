36cb56fc7be0d51009bc3f38566551a12dffefc0
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-02-28 16:05:30 -0500

    Wrote some tests

--- Assignment5/test/AnimationModelImplTest.java
+++ Assignment5/test/AnimationModelImplTest.java
@@ -1,7 +1,10 @@
 import model.*;
+import model.Rectangle;
+import model.Shape;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import static org.junit.Assert.assertEquals;
@@ -13,27 +16,38 @@ public class AnimationModelImplTest {
   @Before
   public void setUp() {
     //example Action objects
-    Action move = new Move("Move", 300, 300);
-    Action changeColor = new ChangeColor("ColorChange", 100,100,100);
-    Action changeDims = new ChangeDims("Dim Change", 100, 200);
+    Action move = new Move(300, 300);
+    Action changeColor = new ChangeColor(100,100,100);
+    Action changeDims = new ChangeDims(100, 200);
 
     //example Shape objects
-    Shape rect = new AbstractShape() //need to make Rectangle class
-    Shape
+    Shape rect = new Rectangle(
+            100,
+            100,
+            new Location(100, 100),
+            new Color(100, 100, 100)); //need to make Rectangle class
+    Shape oval = new Oval(
+            100,
+            100,
+            new Location(200, 200),
+            new Color(200, 200, 200)); //need to make Rectangle class
 
 
-    AnimationModel model = new AnimationModelImpl(500,500,)
+    AnimationModel model = new AnimationModelImpl(new ArrayList<>());
+    model.placeImage(rect);
+    model.placeImage(oval);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testInvalidCanvasSizeHeight() {
-    new AnimationModelImpl(0, 500, new ArrayList<>());
+    new AnimationModelImpl(new ArrayList<>());
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testInvalidCanvasSizeWidth() {
-    new AnimationModelImpl(500, 0, new ArrayList<>());
+    new AnimationModelImpl(new ArrayList<>());
   }
+
 
 
 }
